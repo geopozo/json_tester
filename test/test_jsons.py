536be515e24903643293f7812fe6e82519a3c052
Andrew Pikul <ajpikul@gmail.com>
2025-06-02 18:31:35 -0400

    Get NaN comparison working

--- test/test_jsons.py
+++ test/test_jsons.py
@@ -1,4 +1,6 @@
+import math
 import json
+import pprint
 
 import ujson
 import orjson
@@ -6,11 +8,37 @@ import msgspec
 import pytest
 import simplejson
 
+def assert_almost_equal_with_nan(a, b, fail=None):
+    if not fail:
+        def fail():
+            raise AssertionError(f"{pprint.pformat(a)} != {pprint.pformat(b)}")
+
+    if isinstance(a, float) and isinstance(b, float):
+        if math.isnan(a) and math.isnan(b):
+            return
+        if math.isinf(a) and math.isinf(b) and (a > 0) == (b > 0):
+            return
+        if math.isclose(a, b, rel_tol=1e-9, abs_tol=0.0):
+            return
+        fail()
+    elif isinstance(a, (list, tuple)) and isinstance(b, (list, tuple)):
+        if len(a) != len(b):
+            fail()
+        for i, (x, y) in enumerate(zip(a, b)):
+            assert_almost_equal_with_nan(x, y, fail=fail)
+    elif isinstance(a, dict) and isinstance(b, dict):
+        if a.keys() != b.keys():
+            fail()
+        for k in a:
+            assert_almost_equal_with_nan(a[k], b[k], fail=fail)
+    else:
+        if a != b:
+            fail()
 encoders = {
     "json": lambda data: json.dumps(data),
     "ujson": lambda data: ujson.dumps(data),
     "orjson": lambda data: orjson.dumps(data).decode("utf-8"),
-    "simplejson": lambda data: simplejson.dumps(data),
+    "simplejson": lambda data: simplejson.dumps(data, allow_nan=True),
     "msgspec": lambda data: msgspec.json.encode(data).decode("utf-8"),
 }
 
@@ -18,8 +46,8 @@ decoders = {
     "json": lambda data: json.loads(data),
     "ujson": lambda data: ujson.loads(data),
     "orjson": lambda data: orjson.loads(data.encode("utf-8")),
-    "simplejson": lambda data: simplejson.loads(data),
-    "msgspec": lambda data: msgspec.json.decode(data),
+    "simplejson": lambda data: simplejson.loads(data, allow_nan=True),
+    "msgspec": lambda data: msgspec.json.decode(data, strict=False),
 }
 
 data = {
@@ -38,4 +66,4 @@ def test_encode(benchmark, package, encode_fn, data_type, data):
 def test_decode(benchmark, package, decode_fn, data_type, data):
     benchmark.group=f"decoding_{data_type}"
     encoded = encoders[package](data)
-    assert data == benchmark(decode_fn, encoded)
+    assert_almost_equal_with_nan(data, benchmark(decode_fn, encoded))
