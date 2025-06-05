5c7e78d45e46465ac071654bc82e987f33cc906c
Andrew Pikul <ajpikul@gmail.com>
2025-06-05 01:02:10 -0400

    Add benchmarks to results

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,8 +4,22 @@ import re
 import pytest
 from tabulate import tabulate
 
+def _scale(v):
+    if v >= 1.0:
+        return f"{v:05.1} s"
+    elif v >= 1e-3:
+        return f"{v*1e3:05.1f}ms"
+    elif v >= 1e-6:
+        return f"{v*1e6:05.1f}us"
+    else:
+        return f"{v*1e9:05.1f}ns"
 
-def _color(outcome: str) -> str:
+def _format_result(outcome: str, benchmark_stats=None) -> str:
+    if outcome == "passed" and benchmark_stats:
+        mean = benchmark_stats.mean
+        stddev = benchmark_stats.stddev
+        return f"\x1b[32m{_scale(mean)}±{_scale(stddev)}\x1b[0m"
+
     palette = {
         "passed": "\x1b[32mPASS\x1b[0m",
         "failed": "\x1b[31mFAIL\x1b[0m",
@@ -46,10 +60,19 @@ def pytest_terminal_summary(terminalreporter, exitstatus, config): #noqa: ARG001
             except ValueError:
                 data_type, lib_name = param_str, ""
             lib_names[lib_name] = True
-            # Structure: datatype -> package -> result
+
             datatypes.setdefault(data_type, {}).setdefault(lib_name, {})
 
-            datatypes[data_type][lib_name][test_name] = _color(outcome)
+            bench = None
+            for b in config._benchmarksession.benchmarks:
+                if nodeid.endswith(b.name):
+                    bench = b.stats
+
+            # All need to be equal.
+            datatypes[data_type][lib_name][test_name] = _format_result(
+                outcome,
+                benchmark_stats = bench
+            )
 
     headers = ["Data Type", *sorted(lib_names)]
     rows = []
