c454663594d4fd0ad136e5bc52db7afb7238e58b
Andrew Pikul <ajpikul@gmail.com>
2025-06-06 14:45:17 -0400

    Add aparatus for custom parser

--- tests/serdes.py
+++ tests/serdes.py
@@ -23,7 +23,13 @@ try:
     import ujson
     packages.append("ujson")
 except ImportError:
-    import json
+    pass
+
+try:
+    from custom import custom_dumps, custom_loads
+    packages.append("custom")
+except ImportError:
+    pass
 
 encoders = {
     "json": lambda data: json.dumps(data),
@@ -31,6 +37,7 @@ encoders = {
     "orjson": lambda data: orjson.dumps(data).decode("utf-8"),
     "simplejson": lambda data: simplejson.dumps(data, allow_nan=True),
     "msgspec": lambda data: msgspec.json.encode(data).decode("utf-8"),
+    "custom": lambda data: custom_dumps(data),
 }
 
 decoders = {
@@ -39,4 +46,5 @@ decoders = {
     "orjson": lambda data: orjson.loads(data.encode("utf-8")),
     "simplejson": lambda data: simplejson.loads(data, allow_nan=True),
     "msgspec": lambda data: msgspec.json.decode(data, strict=False),
+    "custom": lambda data: custom_loads(data),
 }
